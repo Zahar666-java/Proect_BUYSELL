0e76119b4da0150eadac39b8f96ee6384933a778
Zahar Prilukov <131271173+Zahar666-java@users.noreply.github.com>
2023-07-06 13:51:55 +0300

    Упростил загрузку фото

--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,28 +27,30 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile... files) throws IOException {
         product.setUser(getUserByPrincipal(principal));
-        Image image1;
-        Image image2;
-        Image image3;
-        if (file1.getSize() != 0) {
-            image1 = toImageEntity(file1);
-            image1.setPreviewImage(true);
-            product.addImageToProduct(image1);
+        List<Image> images = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            if (file.getSize() != 0) {
+                Image image = toImageEntity(file);
+                images.add(image);
+
+                if (images.size() == 1) {
+                    image.setPreviewImage(true);
+                }
+            }
         }
-        if (file2.getSize() != 0) {
-            image2 = toImageEntity(file2);
-            product.addImageToProduct(image2);
-        }
-        if (file3.getSize() != 0) {
-            image3 = toImageEntity(file3);
-            product.addImageToProduct(image3);
-        }
+
+        product.setImages(images);
+
         log.info("Saving new Product. Title: {}; Author email: {}", product.getTitle(), product.getUser().getEmail());
         Product productFromDb = productRepository.save(product);
-        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
-        productRepository.save(product);
+
+        if (!productFromDb.getImages().isEmpty()) {
+            productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
+            productRepository.save(productFromDb);
+        }
     }
 
     public User getUserByPrincipal(Principal principal) {
